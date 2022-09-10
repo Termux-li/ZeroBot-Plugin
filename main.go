05c27db38c2f2a3a9261b7bdab6bef5422c59a93
weigui404 <74092436+weigui404@users.noreply.github.com>
2022-09-10 12:47:09 +0800

    Update main.go

--- main.go
+++ main.go
@@ -42,7 +42,6 @@ import (
 	// 高优先级自定义插件
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/custom"   // 自杀（bushi
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/delreply" // 回复撤回
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/event"    // 事件
 
 	//                               ^^^^                               //
 	//                          ^^^^^^^^^^^^^^                          //

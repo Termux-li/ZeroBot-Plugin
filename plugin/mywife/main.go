b62895ad473ef635c6d21b85644dd61fda07df8e
weigui404 <74092436+weigui404@users.noreply.github.com>
2022-10-10 22:12:53 +0800

    Update main.go

--- plugin/mywife/main.go
+++ plugin/mywife/main.go
@@ -21,10 +21,10 @@ import (
 )
 
 func init() {
-	engine := control.Register("nwife", &ctrl.Options[*zero.Ctx]{
+	engine := control.Register("mywife", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault:  false,
 		Help:              "nativewife\n- 抽老婆[@xxx]\n- 添加wife[名字][图片]\n- 删除wife[名字]\n- [让 | 不让]所有人均可添加wife",
-		PrivateDataFolder: "nwife",
+		PrivateDataFolder: "mywife",
 	})
 	base := engine.DataFolder()
 	baseuri := "file:///" + file.BOTPATH + "/" + base
